0d8b39ee82a4f11fc69d30726f66104ab722d8c8
shahzad.bhatti1 <shahzad.bhatti1@digital.hmrc.gov.uk>
2020-05-15 13:19:49 +0100

    DIT-1997: operational perf test

--- src/test/scala/uk/gov/hmrc/perftests/digitaltariffs/operatorui/OperatorUiSimulation.scala
+++ src/test/scala/uk/gov/hmrc/perftests/digitaltariffs/operatorui/OperatorUiSimulation.scala
@@ -1,7 +1,5 @@
 package uk.gov.hmrc.perftests.digitaltariffs.operatorui
 
-import io.gatling.core.Predef._
-import io.gatling.http.Predef.flushCookieJar
 import io.gatling.http.protocol.HttpProtocolBuilder
 import uk.gov.hmrc.performance.simulation.PerformanceTestRunner
 import uk.gov.hmrc.perftests.digitaltariffs.DigitalTariffsPerformanceTestRunner
@@ -14,40 +12,36 @@ class OperatorUiSimulation extends PerformanceTestRunner with DigitalTariffsPerf
     buildHttpProtocol(url = adminBaseUrl)
   }
 
-  private val scn =
-    scenario("HMRC Operator reviews a BTI application")
-      .exec(flushCookieJar)
-      // Stride Auth Sign In
-      .exec(getProtectedPageNoSession).exec(pause(waitTime))
-      .exec(getStrideSignIn).exec(pause(waitTime))
-      .exec(getIdpSignInPage).exec(pause(waitTime))
-      .exec(postIdpSignInPage).exec(pause(waitTime))
-      .exec(getSignInRedirect).exec(pause(waitTime))
-      .exec(postIdpResponseToStride).exec(pause(waitTime))
+  setup("operationalUI", "HMRC Operator reviews a BTI application") withRequests(
+    // Stride Auth Sign In
+      getProtectedPageNoSession,
+      getStrideSignIn,
+      getIdpSignInPage,
+      postIdpSignInPage,
+      getSignInRedirect,
+      postIdpResponseToStride,
       // HMRC Operator UI journey
-      .exec(getStartPage).exec(pause(waitTime))
-      .exec(getGatewayQueue).exec(pause(waitTime))
-      .exec(getCarsQueue).exec(pause(waitTime))
-      .exec(getStartPage).exec(pause(waitTime))
-      .exec(getGatewayQueue).exec(pause(waitTime))
-      .exec(getCarsQueue).exec(pause(waitTime))
-      .exec(getElmQueue).exec(pause(waitTime))
-      .exec(getActQueue).exec(pause(waitTime))
-      .exec(getCapQueue).exec(pause(waitTime))
-      .exec(getMyCases).exec(pause(waitTime))
-      .exec(findValidCaseReference).exec(pause(waitTime))
-      .exec(getCaseTraderDetails).exec(pause(waitTime))
-      .exec(getCaseApplicationDetails).exec(pause(waitTime))
-      .exec(getCaseAttachments).exec(pause(waitTime))
-      .exec(getCaseActivity).exec(pause(waitTime))
-      .exec(getCaseKeywords).exec(pause(waitTime))
-      .exec(getCaseRuling).exec(pause(waitTime))
-      .exec(searchPage).exec(pause(waitTime))
-      .exec(getQueryResultPage).exec(pause(waitTime))
+      getStartPage,
+      getGatewayQueue,
+      getCarsQueue,
+      getStartPage,
+      getGatewayQueue,
+      getCarsQueue,
+      getElmQueue,
+      getActQueue,
+      getCapQueue,
+      getMyCases,
+      findValidCaseReference,
+      getCaseTraderDetails,
+      getCaseApplicationDetails,
+      getCaseAttachments,
+      getCaseActivity,
+      getCaseKeywords,
+      getCaseRuling,
+      searchPage,
+      getQueryResultPage
+  )
 
-  // TODO: when the `performance-test-runner` is fixed, we should use it here
-    setUp(scn.inject(simulationSteps))
-      .protocols(httpProtocol)
-      .assertions(simulationAssertion)
-
+  runSimulation()
+  
 }
