82b1dc61402813b9f8ee0cfb08b22868dd357660
Kuldeep Wadhwa <11062122+kuldeepwadhwa9@users.noreply.github.com>
2021-11-19 10:06:03 +0000

    DDCE-2473 upgraded performance test runner version (#11)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,11 +5,11 @@ object Dependencies {
   private val gatlingVersion = "3.4.2"
 
   val test = Seq(
-    "com.github.nscala-time" %% "nscala-time"               % "2.28.0" % Test,
-    "com.typesafe"           %  "config"                    % "1.3.3" % Test,
-    "com.typesafe.play"      %% "play-json"                 % "2.6.10" % Test,
+    "com.github.nscala-time" %% "nscala-time"               % "2.30.0" % Test,
+    "com.typesafe"           %  "config"                    % "1.4.1" % Test,
+    "com.typesafe.play"      %% "play-json"                 % "2.9.2" % Test,
     "io.gatling.highcharts"  %  "gatling-charts-highcharts" % gatlingVersion % Test,
     "io.gatling"             %  "gatling-test-framework"    % gatlingVersion % Test,
-    "uk.gov.hmrc"            %% "performance-test-runner"   % "4.11.0" % Test
+    "uk.gov.hmrc"            %% "performance-test-runner"   % "5.0.0" % Test
   )
 }
